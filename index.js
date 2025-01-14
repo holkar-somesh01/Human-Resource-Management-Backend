9a3d10d2b0fc506757893371413298b11139f018
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-01-14 13:33:57 +0530

    env varivables

--- index.js
+++ index.js
@@ -27,7 +27,6 @@ app.use("/api/teamlead", require("./routes/teamLead.routes"))
 
 app.use("*", (req, res) => {
     res.sendFile(path.join(__dirname, "dist", "index.html"))
-    // res.status(404).json({ message: "Resours Not found" })
 })
 app.use((err, req, res, next) => {
     console.log(err)
