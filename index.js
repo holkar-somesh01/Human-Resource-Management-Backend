9ffc6ef7dd41b4428a6247880bec5aefd81ee519
someshwarsholkar <someshwarsholkar22@gmail.com>
2025-01-16 11:43:36 +0530

    env Variable Manage

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cors({
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.static("dist"))
-app.use(express.static("excelSheet"))
 
 app.use("/api/auth", require("./routes/auth.routes"))
 app.use("/api/employee", require("./routes/employee.routes"))
